9d44392866c737ae87051a291bc66a9d936b8b39
Vaishnavi Rangarajan <r.vaishnavi15@gmail.com>
2024-08-08 19:05:54 -0700

    Texture and stars

--- src/App.jsx
+++ src/App.jsx
@@ -2,17 +2,22 @@ import { useState, useRef } from 'react'
 import './App.css'
 import Globe from './Globe'
 import { Canvas } from '@react-three/fiber'
+import { Stars } from '@react-three/drei'
 
 function App() {
   const [count, setCount] = useState(0)
 
+
+  // Texture maps from https://planetpixelemporium.com/earth8081.html
   return (
  
 <Canvas> 
 
 <ambientLight intensity={0.5}/>
+
 <directionalLight position={[5,5,5]} intensity={1} />
 <Globe />
+<Stars />
 </Canvas>
   
   )
--- src/Globe.jsx
+++ src/Globe.jsx
@@ -1,9 +1,13 @@
-import { Canvas, useFrame } from '@react-three/fiber'
+import { OrbitControls } from '@react-three/drei';
+import { Canvas, useFrame, useLoader } from '@react-three/fiber'
 import React, { useRef } from 'react'
+import { TextureLoader } from 'three';
 
 
 const Globe = () => {
     const ref = useRef();
+    const texture = useLoader(TextureLoader, '/earth_texture.jpg');
+    const bump = useLoader(TextureLoader, 'earth_bump.jpg')
 
     useFrame(()=>{
         ref.current.rotation.y += 0.001;
@@ -11,8 +15,9 @@ const Globe = () => {
 
   return (
 <mesh ref={ref} >
-    <sphereGeometry args = {[1,32,32]} />
-    <meshStandardMaterial color="blue" />
+    <OrbitControls />
+    <sphereGeometry args = {[2,32,32]} />
+    <meshStandardMaterial map={texture} bumpMap={bump} bumpScale={0.5}/>
 </mesh>
   )
 }
